48cf9198ea4bfc8f30e27a4262e9e336311c750b
Antônio Antunes <antunesantonio91@gmail.com>
2024-08-02 10:14:39 -0300

    atualizando página e estilo

--- tarefas/urls.py
+++ tarefas/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path('', views.index, name="index"),
+    path('jogadores/', views.jogadores, name="jogadores"),
+    path('sobre/', views.sobre, name="sobre"),
 ]
