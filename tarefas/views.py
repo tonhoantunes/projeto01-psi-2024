702d45c01f6a5d0e673337e964a1d22bf287c1d6
Antônio Antunes <antunesantonio91@gmail.com>
2024-08-07 16:19:11 -0300

    menu de navegação

--- tarefas/views.py
+++ tarefas/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, "index.html")
+
+def jogadores(request):
+    return render(request, "jogadores.html")
+
+def sobre(request):
+    return render(request, "sobre.html")
